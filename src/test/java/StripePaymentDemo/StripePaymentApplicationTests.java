bfd091cb8bc194292e066dad40c5592d7d7bfe7b
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-12-29 11:14:27 +0700

    Init

---
+++ src/test/java/StripePaymentDemo/StripePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package StripePaymentDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
